2be2bd0453867293b6e44dc61c1d57064c4af82a
AF-30 <grandahmad40@gmail.com>
2022-10-04 14:12:24 +0300

    Initial Commit

---
+++ Fruzzdigital/FruzzdigitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruzzdigitalApp.swift
+//  Fruzzdigital
+//
+//  Created by grand ahmad on 08/03/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FruzzdigitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
